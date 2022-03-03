79a669b14865a06347778142f2d903a4178db4fe
zcKO <studyJcHub@gmail.com>
2022-03-03 10:16:58 +0900

    버튼 클릭시 로또 구매 로직 시작  - 코틀린의 for 문

--- app/src/main/java/com/jc/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/jc/lottosimulator_20220303/MainActivity.kt
@@ -2,10 +2,45 @@ package com.jc.lottosimulator_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setValues()
+        setupEvents()
+
     }
+
+    private fun setValues() {
+
+
+    }
+
+    private fun setupEvents() {
+
+        btnBuyLotto.setOnClickListener {
+            // 6개의 당첨 번호 생성
+            // 코틀린의 for 문은 for-each 문법 기반.
+            for(i in 1..5) {
+                Log.d("반복문 확인", "setupEvents: $i")
+            }
+
+            // 만들어진 당첨 번호 6개 -> TextView 에 표현
+
+            // 보너스 번호 생성
+
+            // 생성된 보너스 번호 TextView 에 배치
+
+        }
+    }
+
+    private fun buyLotto() {
+
+    }
+
 }
